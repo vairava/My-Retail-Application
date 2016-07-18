4ee67955b9b0ddf4e80ccf761a3618ef501edc77
VXV8792 <vairava_vigneswaran@homedepot.com>
2016-07-18 18:48:17 -0400

    view all db data

--- src/com/work/myretail/util/GenerateTOHelper.java
+++ src/com/work/myretail/util/GenerateTOHelper.java
@@ -12,11 +12,11 @@ public class GenerateTOHelper {
 	public static void main(String[] args) throws UnknownHostException {
 		
 		DBCollection col=MongoDBConnection.getDBConnection();
-		PricingInsertTO pricingTo=new PricingInsertTO();
-		pricingTo.setId(13860428);
-		pricingTo.setProductName("BIG LEBOWSKI, THE Blu-ray");;
-		pricingTo.setValue(99.10);
-		pricingTo.setCurrency_code("RUPEE");
+//		PricingInsertTO pricingTo=new PricingInsertTO();
+//		pricingTo.setId(13860428);
+//		pricingTo.setProductName("BIG LEBOWSKI, THE Blu-ray");;
+//		pricingTo.setValue(99.10);
+//		pricingTo.setCurrency_code("RUPEE");
 		//MongoDBConnection.createData(col, pricingTo);
 		//MongoDBConnection.insertData(col, pricingTo);
 		//MongoDBConnection.deleteData(col, pricingTo.getId());
